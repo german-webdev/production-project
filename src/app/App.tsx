b1cd090d2ce107f5e8f3cd98d131a8a7f90cde67
german-webdev <frontendue@gmail.com>
2024-02-05 00:22:05 +1000

    added fsd and absolute paths

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,11 @@ import { FC, Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom';
 
 import './styles/index.scss';
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+
 
 
 
@@ -22,8 +23,8 @@ export const App: FC = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageLazy />}/>
-          <Route path={'/'} element={<MainPageLazy />}/>
+          <Route path={'/about'} element={<AboutPage />}/>
+          <Route path={'/'} element={<MainPage />}/>
         </Routes>
       </Suspense>
       
