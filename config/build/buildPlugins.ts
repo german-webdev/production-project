99374bbcbab8165b2d9c1342b023db6163dda5a7
german-webdev <frontendue@gmail.com>
2024-02-22 15:32:47 +1000

    add BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import webpack from 'webpack';
 import { type BuildOptions } from './types/config';
 
@@ -17,6 +18,9 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
       __IS_DEV__: JSON.stringify(isDev)
     }),
     isDev && new ReactRefreshWebpackPlugin(),
-    isDev && new webpack.HotModuleReplacementPlugin()
+    isDev && new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ];
 }
