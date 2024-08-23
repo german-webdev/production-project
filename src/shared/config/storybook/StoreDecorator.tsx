3c2a06062f126ca48e25d7c1ec74ae3e6f903895
german-webdev <frontendue@gmail.com>
2024-08-24 00:24:02 +1000

    hotfix for lint:ts

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -1,7 +1,6 @@
 import { type StoryFn } from '@storybook/react';
-import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from 'app/types/DeepPartial';
-
+import { type StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { type DeepPartial } from 'app/types/DeepPartial';
 
 const StoreDecorator = (initialState: DeepPartial<StateSchema>) => (Story: StoryFn) => {
   return (
