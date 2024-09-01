e505a60ce0235e57d5da935b5cb90a50aeb2ac8f
german-webdev <frontendue@gmail.com>
2024-09-01 22:27:37 +1000

    add LoginForm, Input stories

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,22 +1,12 @@
-import { Input } from 'shared/ui/Input/Input';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
-  const [value, setValue] = useState('');
-
-  const onChange = (value: string) => {
-    setValue(value);
-  };
 
   return (
     <div>
       {t('Главная страница')}
-      <Input
-        placeholder='Введите текст'
-        value={value}
-        onChange={onChange}/>
     </div>
   );
 };
---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './Input';
+
+const meta = {
+  title: 'shared/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Type text',
+    value: '123123'
+  }
+};
